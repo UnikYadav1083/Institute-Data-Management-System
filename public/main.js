cb9c14b937cbc3765325ef2168593e691a1b54ce
UnikYadav1083 <132219783+UnikYadav1083@users.noreply.github.com>
2023-04-30 22:21:43 +0530

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById('showBtn')
+let opts = document.getElementById('tables')
+
+// btn.onclick = () => {
+//     window.location.replace('http://localhost:4000/see/query?table=' + opts.value)
+// }
